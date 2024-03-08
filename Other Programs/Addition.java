06fc0f82a947391137beca56074861b31a81f43a
Angel <135215939+AngelKA00@users.noreply.github.com>
2024-03-08 12:46:29 +0530

    Add files via upload

---
+++ Other Programs/Addition.java
@@ -0,0 +1,10 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 14, b = 2, c = 2000;
+		int result = a + b + c;
+		System.out.print("Result: " +result);
+	}
+}
